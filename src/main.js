ad1c9054afbd1c6bc7ae694c8e20103cdaee58a4
Richard Snider <richardnsnider@gmail.com>
2021-11-18 12:57:56 -0800

    add exec functionality with scrubbing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,57 @@
-/**
- * @typedef {Object} Module
- * @property {function(number,number):number|function(string,string):string} add
+const execSync = require('child_process').execSync;
+const loop = require('@vorprog/loop').loop;
+
+/** @param {string} content @param {string[]} secrets @returns {string} */
+const scrub = (content, secrets) => {
+  loop(secrets, secret => content = content.replace(secret, `****REDACTED****`));
+  return content;
+};
+
+/** @param {string} content */
+const parseJson = content => {
+  try {
+    return JSON.parse(content);
+  } catch (error) {
+    return content;
+  }
+};
+
+/** 
+ * @typedef {Object} ExecOptions
+ * @property {string[]} ignorableErrors
+ * @property {string[]} secrets
+ * @property {function(...any):any} logFunction(bla)
  */
 
-/** @type {Module} */
+/** @type {ExecOptions} */
+let defaultOptions = {
+  logFunction: (...data) => {
+    console.log(...data);
+    return data;
+  }
+};
+
 module.exports = {
-  add: (arg0, arg1) => arg0 + arg1
-}
+  /** @param {ExecOptions} options */
+  setDefaultOptions: (options) => defaultOptions = Object.assign(defaultOptions, options),
+
+  /** @param {string} command @param {ExecOptions} options */
+  exec: (command, options = {}) => {
+    const stackTrace = new Error().stack.split(`at `)[2].replace(`Object.<anonymous>`, ``).trim();
+    const currentOptions = Object.assign(defaultOptions, options);
+
+    try {
+      command = command.replace(/\n/g, ` `); // replace new lines with spaces to keep the command on a single line
+      if (currentOptions.logFunction) currentOptions.logFunction(`\nTRACE: ${stackTrace}`, scrub(command, currentOptions.secrets));
+      const result = execSync(command, { stdio: 'pipe' }).toString();
+      return parseJson(result);
+    } catch (error) {
+      if (error instanceof Error) {
+        const scrubbedErrorMessage = scrub(error.message, currentOptions.secrets);
+        let throwError = true;
+        loop(currentOptions.ignorableErrors, ignorableError => scrubbedErrorMessage.includes(ignorableError) ? throwError = false : null);
+        if (throwError) throw new Error(scrubbedErrorMessage);
+      }
+    }
+  }
+};
--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,32 @@
 const diff = require('diff');
 const loop = require('@vorprog/loop').loop;
-const main = require('../src/main');
+const exec = require('../src/main');
 
-const testResults1 = diff.diffWords(`33`, main.add(11, 22).toString());
-if (testResults1.length > 1) {
-  const diffLog = loop(testResults1, (index, change) => `${change.added ? `DISCREPANCY.ACTUAL:` : change.removed ? `DISCREPANCY.EXPECTED:` : ``} ${change.value}`).join(`\n`);
-  throw Error(`There were unexpected results during testing: \n${diffLog}`);
-}
+const testStrings = (testName, expected, actual) => {
+  const testResults = diff.diffWords(expected, actual);
 
-const testResults2 = diff.diffWords(`hello world!`, main.add(`hello `, `world!`));
-if (testResults2.length > 1) {
-  const diffLog = loop(testResults2, (index, change) => `${change.added ? `DISCREPANCY.ACTUAL:` : change.removed ? `DISCREPANCY.EXPECTED:` : ``} ${change.value}`).join(`\n`);
-  throw Error(`There were unexpected results during testing: \n${diffLog}`);
-}
+  if (testResults.length <= 1) {
+    console.log(`Test "${testName}" passed`);
+    return;
+  }
 
-console.log(`Tests successfully completed!`);
+  const diffLog = loop(testResults, (index, change) => {
+    const changeType = change.added ? `ACTUAL:` : change.removed ? `EXPECTED:` : ``;
+    return (`${changeType} ${change.value}`)
+  }).join(`\n`);
+  console.error(`Test "${testName}" failed. There were unexpected results during testing: \n${diffLog}`);
+};
+
+const commandResult1 = exec.exec(`echo "hello world"`);
+
+testStrings(`test#1`, `hello world`, commandResult1);
+
+exec.setDefaultOptions({
+  secrets: [`pass1234`, `\\az190-#%$^&{}*!@=+?<>//`],
+  ignorableErrors: [
+    `An error occurred (EntityAlreadyExists)`,
+  ]
+});
+
+const commandResult2 = exec.exec(`echo "Hello World! The secrets are pass1234 and \\az190-#%$^&{}*!@=+?<>//"`);
+testStrings(`test#2`,`Hello World! The secrets are ****REDACTED**** and ****REDACTED****`, commandResult2);
